642aaf829b112397ab2158a2ff2a9963e24da0fd
Andriy007i <andrii.zub@nure.ua>
2025-04-27 19:28:47 +0300

    Update and rename Universitet.cs to University.cs

---
+++ Курсовая/Modules/University.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Курсовая.Modules
+{
+    public class University
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Adress { get; set; }
+        public List<Specialty> Specialties { get; set; } = new List<Specialty>();
+        public string PhoneNumber { get; set; }
+        
+        public University()
+        {
+            Specialties = new List<Specialty>();
+        }
+
+        public void AddSpecialty(Specialty specialty)
+        {
+            if (specialty != null && !Specialties.Contains(specialty))
+            {
+                Specialties.Add(specialty);
+        }
+
+        public override string ToString()
+        {
+            return $"University: {Name}, City: {City}, Adress: {Adress}, Phone: {PhoneNumber}";
+        }
+    }
+}
