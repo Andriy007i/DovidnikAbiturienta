6d3be9fb84f230c37ca287ca56fdc66101f38349
Andriy007i <andrii.zub@nure.ua>
2025-04-27 20:21:16 +0300

    Update UniversityRepository.cs

--- Курсовая/Modules/UniversityRepository.cs
+++ Курсовая/Modules/UniversityRepository.cs
@@ -9,42 +9,49 @@ namespace Курсовая.Modules
 {
     public class UniversityFacade
     {
-        public List<University> Universities { get; private set; }
+        public List<University> universities;
 
         public UniversityRepository()
         {
-            Universities = new List<University>();
+            universities = new List<University>();
             SeedData();
         }
+        
         private void SeedData()
         {
-            var univer1 = new Universitet("Київський національний університет", "Київ" ,"проспект Перемоги, 37","+380995485775");
-            univer1.Specialties.Add(new Specialty("Комп'ютерні науки", 122, 300, 25000));
-            univer1.Specialties.Add(new Specialty("Юридичні науки", 112, 200, 40000));
+            var knu = new Universitet("Київський національний університет", "Київ" ,"проспект Перемоги, 37","+380995485775");
+            univer1.Specialties.Add(new Specialty("Комп'ютерні науки", 122, 300, 100, 25000));
+            univer1.Specialties.Add(new Specialty("Юридичні науки", 112, 200, 50, 40000));
 
-            var univer2 = new Universitet("Харківський національний університет радіоелектроніки", "Харків");
-            univer1.Specialties.Add(new Specialty("Комп'ютерна інженерія", 123, 50, 50000));
-            univer1.Specialties.Add(new Specialty("Інженерія програмного забезпечення", 121, 40, 70000));
+            var khnure = new Universitet("Харківський національний університет радіоелектроніки", "Харків", "проспект Науки, 14","0502805373");
+            univer1.Specialties.Add(new Specialty("Комп'ютерна інженерія", 123, 50, 20, 50000));
+            univer1.Specialties.Add(new Specialty("Інженерія програмного забезпечення", 121, 40, 10, 70000));
 
-            Universities.Add(univer1);
-            Universities.Add(univer2);
+            Universities.Add(knu);
+            Universities.Add(khnure);
         }
-        public University GetUniversityByName(string name)
+
+        public List<University> GetAll()
         {
-            return Universities.FirstOrDefault(u => u.Name == name);
+            return universities;
         }
         
-        public List<University> GetSpecialtiesByName(string specialtyName)
+        public University GetUniversityByName(string name)
+        {
+            return Universities.FirstOrDefault(u => u.Name.ToLower() == name.ToLower());
+        }
+        
+        public List<Specialty> GetSpecialtiesByName(string specialtyName)
         {
             return universities
                 .SelectMany(u => u.Specialties)
-                .Where(s => s.Name == specialtyName)
+                .Where(s => s.Name.ToLower() == specialtyName.ToLower())
                 .ToList();
         }
         public Specialty GetSpecialtyWithMinimalCompetition(string SpecialtyName)
         {
             return GetSpecialtiesByName(specialtyName)
-                .OrderBy(s => s.Competition)
+                .OrderBy(s => s.DayTimeCompetition)
                 .FirstOrDefault():
         }
         }
